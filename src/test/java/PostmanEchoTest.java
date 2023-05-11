fa7c264e0c8788fe2d6ab2543c657f3772c60042
A. Stefanovich <stefanovich.anastasiya@yandex.ru>
2023-05-12 01:27:16 +0300

    init

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    // Given - When - Then
+// Предусловия
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("today is beautiful day") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("today is beautiful day"))
+        ;
+    }
+}
